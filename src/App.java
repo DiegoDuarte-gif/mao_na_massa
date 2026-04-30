dd7153d15d5e099dba4dc15aa040476870e4e4f6
DiegoDuarte-gif <diegoduarte0n@gmail.com>
2026-04-30 10:40:37 -0300

    mao_na_massa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = scanner.nextInt();
+        boolean resultado = ehPar(num);
+        if (resultado == true) {
+            System.out.println("Esse número é par!");}
+            else {System.out.println("Esse número é ímpar!");
+        }
+     scanner.close();
     }
-}
+    public static boolean ehPar(int num){
+        return (num %2==0);
+        }
+    }
